ddc4a753611e1e7232db93011360019d8c133405
Aurimas Tarasevičius <autaras@gmail.com>
2020-07-30 00:31:25 +0300

    fixing failing unit tests

--- hangman_project/hangman_game/models.py
+++ hangman_project/hangman_game/models.py
@@ -1,21 +1,2 @@
 from django.db import models
 
-
-class WordBank(models.Model):
-    word = models.CharField(max_length=200, unique=False)
-
-    def __str__(self):
-        return self.word
-
-    objects = models.Manager()
-
-
-class GameState(models.Model):
-    game_status = models.SmallIntegerField()
-    word_to_guess_id = models.ForeignKey(WordBank, on_delete=models.CASCADE)
-    word_mask = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.game_status
-
-    objects = models.Manager()
