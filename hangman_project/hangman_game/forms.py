b503a7bbbf85cea9b593175a4271581e2f542a35
Aurimas Tarasevičius <autaras@gmail.com>
2020-08-05 23:05:36 +0300

    hardcoded target language options for translations

--- hangman_project/hangman_game/forms.py
+++ hangman_project/hangman_game/forms.py
@@ -1,24 +1,24 @@
 from django import forms
 
-DIFFICULTY_CHOICES = [(1, "Easy"), (2, "Normal"), (3, "Hard")]
-
 
 class OptionsForm(forms.Form):
+    __DIFFICULTY_CHOICES = [(1, "Easy"), (2, "Normal"), (3, "Hard")]
+
     difficulty_level = forms.ChoiceField(
         label="Select game difficulty",
-        widget=forms.RadioSelect(attrs={
-            # "class": "form-check-input",
-        }),
-        choices=DIFFICULTY_CHOICES,
+        widget=forms.RadioSelect(),
+        choices=__DIFFICULTY_CHOICES,
     )
     translate_word = forms.BooleanField(
         label="Translate the word in the end of the game.",
-        widget=forms.CheckboxInput(attrs={
-            #
-        }),
+        widget=forms.CheckboxInput(),
         required=False
     )
 
-    target_language = forms.CharField(
-        label="Select target language to translate",
+    __TARGET_LANGUAGES = [('lt', "Lithuanian"), ('en', "English"), ('de', "German"), ('ru', "Russian")]
+
+    target_language = forms.ChoiceField(
+        label="Select target language for translation",
+        widget=forms.RadioSelect(),
+        choices=__TARGET_LANGUAGES,
     )
