25c0e0f081fefb309d4b84d20e310c0bfca8e958
Aurimas Tarasevičius <autaras@gmail.com>
2020-08-18 22:51:20 +0300

    cleanup not needed applications

--- hangman_project/hangman_project/settings.py
+++ hangman_project/hangman_project/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'hangman_game.apps.HangmanGameConfig',
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
