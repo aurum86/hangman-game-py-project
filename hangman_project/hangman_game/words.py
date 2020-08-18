f053650ac4549eb15ec79ef8df5f4f9f5e44e152
Aurimas Tarasevičius <autaras@gmail.com>
2020-08-18 22:32:00 +0300

    fix regex warning

--- hangman_project/hangman_game/words.py
+++ hangman_project/hangman_game/words.py
@@ -8,7 +8,7 @@ class WordBank:
     @classmethod
     def get_word_list(cls) -> list:
         text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere justo orci, in mollis ex porta in. Pellentesque ullamcorper libero vel eros volutpat, sed sagittis ligula lacinia. Ut vel mollis metus. Aliquam quis nulla id eros tincidunt ultricies. Fusce sodales quis ante ut egestas. In tristique faucibus felis, sit amet."
-        words = re.sub("\W", " ", text)
+        words = re.sub(r"\W", " ", text)
 
         return words.split()
 
